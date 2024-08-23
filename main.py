a3840b349d0627588cbe81099e1864b1bd23657f
Amajing102 <mdarwisya10@gmail.com>
2024-08-23 10:10:59 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+Bilangan_betul = 0
+Nombor_1 = 0
+Nombor_2 = 0
+Jawapan = 0
+
+def on_button_pressed_a():
+    global Bilangan_betul, Nombor_1, Nombor_2, Jawapan
+    Bilangan_betul = 0
+    Nombor_1 = 0
+    Nombor_2 = 0
+    Jawapan = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let Bilangan_betul = 0
+let Nombor_1 = 0
+let Nombor_2 = 0
+let Jawapan = 0
 basic.forever(function () {
-	
+    Bilangan_betul = 0
+    Nombor_1 = 0
+    Nombor_2 = 0
+    Jawapan = 0
+    basic.showNumber(Bilangan_betul)
+    basic.showNumber(Nombor_1)
+    basic.showNumber(Nombor_2)
+    Nombor_1 = randint(1, 20)
+    Nombor_2 = randint(1, 20)
+    Jawapan = Nombor_1 * Nombor_2
 })
